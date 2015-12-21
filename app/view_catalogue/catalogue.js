a32a54ea607abb937d1acbb0d247aa403a5e5f2c
Cesar Velandia <cesar@velandia.co>
2015-12-21 13:31:50 +0800

    Add images

--- app/view_catalogue/catalogue.js
+++ app/view_catalogue/catalogue.js
@@ -7,12 +7,22 @@
 			   		canPurchase: false,
 			   		soldOut: false,
 			   		description: 'Some gems have hidden qualities...',
+			   		images: [
+			   			"http://placehold.it/350x150",
+			   			"http://placehold.it/140x100",
+			   			"http://placehold.it/200x100"
+			   		]
 			  },
 			  {		name: 'Ruby',
 			   		price: 12.95,
 			   		canPurchase: true,
 			   		soldOut: false,
 			   		description: 'Some gems have hidden qualities...',
+			   		images: [
+			   			"http://placehold.it/350x150",
+			   			"http://placehold.it/140x100",
+			   			"http://placehold.it/200x100"
+			   		]
 			  }];
 
 	var app = angular.module('myApp.catalogue', ['ngRoute'])
