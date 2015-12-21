e74470784e62ec2372494dd81732041c60252a42
Cesar Velandia <cesar@velandia.co>
2015-12-21 12:33:51 +0800

    Add simple directives and multiple products

--- app/view_catalogue/catalogue.js
+++ app/view_catalogue/catalogue.js
@@ -2,11 +2,23 @@
 
 (function(){
 
-	var gem = {name: 'Azurite', price: 2.95};
+	var gems = [{	name: 'Azurite',
+			   		price: 2.95,
+			   		canPurchase: false,
+			   		soldOut: false,
+			   		description: 'Some gems have hidden qualities...',
+			  },
+			  {		name: 'Ruby',
+			   		price: 12.95,
+			   		canPurchase: true,
+			   		soldOut: false,
+			   		description: 'Some gems have hidden qualities...',
+			  }];
+
 	var app = angular.module('myApp.catalogue', ['ngRoute'])
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	})
 
 	app.config(['$routeProvider', function($routeProvider){
